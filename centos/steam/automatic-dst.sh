21478a5026968b0ee0f1ecf08dfc98c2e02a5957
shunop <74529141+shunop@users.noreply.github.com>
2021-01-24 23:35:41 +0800

    feat:format style

--- centos/steam/automatic-dst.sh
+++ centos/steam/automatic-dst.sh
@@ -6,139 +6,122 @@
 ## Version： v1.0.0
 ## Description: Auto save|shutdown|update|restart dst.
 
-screen_name_1=$"master"
-screen_name_2=$"caves"
-screen_name_3=$"test"
+v_screen_master=$"master"
+v_screen_caves=$"caves"
 
-function exists_screen() {
-  err=$"--err.exists_screen:"
-  log=$"--log.exists_screen:"
-  if [ 1 -ne $# ]; then
-    echo -e "$err Must have one and only one parameter "
-    return 99
-  fi
-  echo -e "$log input parameter1: $1"
-  echo -e $(screen -ls | grep -e "$1")
+function f_exists_screen() {
+  v_screen_name="$1"
+  echo $(screen -ls | grep -e "$v_screen_name")
   ## -n 非空真 The not null is true.  -z 空为真 The null is true.
-  if [[ -n $(screen -ls | grep -e "$1") ]]; then
-    echo -e "$log have screen: $1"
+  if [[ -n $(screen -ls | grep -e "$v_screen_name") ]]; then
+    echo "have screen: $v_screen_name"
     return 0
   else
-    echo -e "$log dont have screen: $1"
+    echo "dont have screen: $v_screen_name"
     return 1
   fi
 }
 
-function save_dst() {
-  err=$"--err.save_dst:"
-  log=$"--log.save_dst:"
-  cmd_save=$"c_save() \n"
+function f_save_dst() {
+  v_screen_name="$1"
+  v_cmd_save=$"c_save() \n"
   ## Determine if the screen exists.
-  exists_screen "$1"
+  f_exists_screen "$v_screen_name"
   if [[ 0 -eq $? ]]; then
-    screen -S $1 -p 0 -X stuff "$cmd_save"
-    echo -e "$log save $1"
+    screen -S $v_screen_name -p 0 -X stuff "$v_cmd_save"
+    echo "save: $v_screen_name"
     sleep 30s ## This process sleeps for 30 seconds.
   fi
 }
 
-function shutdown_dst() {
-  err=$"--err.shutdown_dst:"
-  log=$"--log.shutdown_dst:"
-  cmd_shutdown=$"c_shutdown() \n"
+function f_shutdown_dst() {
+  v_screen_name="$1"
+  v_cmd_shutdown=$"c_shutdown() \n"
   ## Determine if the screen exists.
-  exists_screen "$1"
+  f_exists_screen "$v_screen_name"
   if [[ 0 -eq $? ]]; then
-    screen -S $1 -p 0 -X stuff "$cmd_shutdown"
-    echo -e "$log shutdown $1"
-    #tail_server_log "$1";
-    #res1=$(screen -S $1 -p 0 -X stuff $"ls \n");
+    screen -S $v_screen_name -p 0 -X stuff "$v_cmd_shutdown"
+    echo "shutdown: $v_screen_name"
+    #f_tail_server_log "$v_screen_name";
+    #res1=$(screen -S $v_screen_name -p 0 -X stuff $"ls \n");
     #echo -e "$res1";
   fi
 }
 
-function send_announce() {
-  log=$"--log.send_announce:"
-  cmd_announce=$"c_announce(\"10s后服务器更新重启 q群 \") \n"
+function f_send_announce() {
+  v_screen_name="$1"
+  v_cmd_announce=$"c_announce(\"10s后服务器更新重启 q群 \") \n"
   ## Determine if the screen exists.
-  exists_screen "$1"
+  f_exists_screen "$v_screen_name"
   if [[ 0 -eq $? ]]; then
-    screen -S $1 -p 0 -X stuff "$cmd_announce"
-    echo -e "$log $1 send $cmd_announce"
-    #tail_server_log "$1";
-    #res1=$(screen -S $1 -p 0 -X stuff $"ls \n");
+    screen -S $v_screen_name -p 0 -X stuff "$v_cmd_announce"
+    echo "send: $v_screen_name $v_cmd_announce"
+    #f_tail_server_log "$v_screen_name";
+    #res1=$(screen -S $v_screen_name -p 0 -X stuff $"ls \n");
     #echo -e "$res1";
   fi
 }
 
-function tail_server_log() {
-  err=$"--err.tail_server_log:"
-  log=$"--log.tail_server_log:"
-  master_server_log=$"/home/steamgame/dst/dst/World1/Master/server_log.txt"
-  caves_server_log=$"/home/steamgame/dst/dst/World1/Caves/server_log.txt"
-  server_log=$"/home/steamgame/dst/dst/World1/Master/server_log.txt"
+function f_tail_server_log() {
+  v_screen_name="$1"
+  v_server_log_file=$"/home/steamgame/dst/dst/World1/Master/server_log.txt"
   ## Replace command
-  if [ "$screen_name_1" = "$1" ]; then
-    server_log="$master_server_log"
-  elif [ "$screen_name_2" = "$1" ]; then
-    server_log="$caves_server_log"
-  else
-    echo -e "$err Invalid paramter "
-    exit
+  if [ "$v_screen_caves" = "$v_screen_name" ]; then
+    v_server_log_file="/home/steamgame/dst/dst/World1/Caves/server_log.txt"
   fi
-
-  if [ ! -e $server_log ]; then
-    echo -e "$log The file is not exist"
+  ## check file
+  if [ ! -f $v_server_log_file ]; then
+    echo "[$v_server_log_file]The file is not exist"
     return 99
   fi
-  echo -e "$log wait..."
-  while true; do
-    if [[ -n $(tail $server_log | grep 'Shutting down') ]]; then
+
+  echo -e "wait..."
+  ## 获取从1970-01-01 00:00:00 UTC到现在的秒数
+  ## v_cur_sec=$(date '+%s')
+
+  ## 300s overtime
+  for i in $(seq 1 300); do
+    if [[ -n $(tail $v_server_log_file | grep -i 'Shutting down') ]]; then
       break
     else
-      #echo -e '--sleep';
       sleep 1s
     fi
   done
-  echo -e "$log the end"
+  echo -e "the end"
 }
 
-function start_dst() {
-  err=$"--err.start_dst:"
-  log=$"--log.start_dst:"
+function f_start_dst() {
+  v_screen_name="$1"
   cmd_cd=$"cd /home/steamgame/dst/bin/ \n"
   cmd_master=$"sh master_start.sh \n"
   cmd_caves=$"sh cave_start.sh \n"
   cmd_start_sh=$"ls \n"
-  if [ 1 -gt $# ]; then
-    echo -e "$err At least one parameter "
-    exit
-  fi
+
   ## Replace command
-  if [ "$screen_name_1" = "$1" ]; then
+  if [ "$v_screen_master" = "$v_screen_name" ]; then
     cmd_start_sh="$cmd_master"
-  elif [ "$screen_name_2" = "$1" ]; then
+  elif [ "$v_screen_caves" = "$v_screen_name" ]; then
     cmd_start_sh="$cmd_caves"
   else
     echo -e "$err Invalid paramter "
-    exit
+    exit 1
   fi
   ## Determine if the screen exists
-  exists_screen "$1"
+  f_exists_screen "$v_screen_name"
   if [[ 0 -ne $? ]]; then
     ## create screen and detach it
     if [ "init" = "$2" ]; then
-      echo -e "$log create the screen: $1"
-      screen -dmS $1
+      echo -e "create the screen: $v_screen_name"
+      screen -dmS $v_screen_name
     else
-      echo -e "$log The screen is not exists and param is not init: $1 "
-      return 99
+      echo -e "The screen is not exists and param is not init: $v_screen_name "
+      exit 1
     fi
   fi
   ## Send commands to the offline screen
-  screen -S $1 -p 0 -X stuff "$cmd_cd"
-  screen -S $1 -p 0 -X stuff "$cmd_start_sh"
-  echo -e "$log end"
+  screen -S $v_screen_name -p 0 -X stuff "$cmd_cd"
+  screen -S $v_screen_name -p 0 -X stuff "$cmd_start_sh"
+  echo -e "end"
 }
 
 function update_dst() {
@@ -152,35 +135,35 @@ echo $(date +%F%n%T)
 echo -e '--log.main: input parameter1: '$1
 
 if [ "initmaster" = "$1" ]; then
-  start_dst "$screen_name_1" init
+  f_start_dst "$v_screen_master" init
 elif [ "initcaves" = "$1" ]; then
-  start_dst "$screen_name_2" init
+  f_start_dst "$v_screen_caves" init
 elif [ "restart" = "$1" ]; then
-  start_dst "$screen_name_2"
-  start_dst "$screen_name_1"
+  f_start_dst "$v_screen_caves"
+  f_start_dst "$v_screen_master"
 elif [ "shutdown" = "$1" ]; then
-  send_announce "$screen_name_1"
-  send_announce "$screen_name_2"
+  f_send_announce "$v_screen_master"
+  f_send_announce "$v_screen_caves"
   sleep 10s
-  save_dst "$screen_name_1"
-  shutdown_dst "$screen_name_2"
-  shutdown_dst "$screen_name_1"
-  tail_server_log "$screen_name_1"
+  f_save_dst "$v_screen_master"
+  f_shutdown_dst "$v_screen_caves"
+  f_shutdown_dst "$v_screen_master"
+  f_tail_server_log "$v_screen_master"
   #echo -e 'The result:'$?;
 elif [ "updatedst" = "$1" ]; then
   update_dst
 elif [ "SUR" = "$1" ]; then
-  send_announce "$screen_name_1"
-  send_announce "$screen_name_2"
+  f_send_announce "$v_screen_master"
+  f_send_announce "$v_screen_caves"
   sleep 10s
-  save_dst "$screen_name_1"
-  shutdown_dst "$screen_name_2"
-  shutdown_dst "$screen_name_1"
-  tail_server_log "$screen_name_1"
+  f_save_dst "$v_screen_master"
+  f_shutdown_dst "$v_screen_caves"
+  f_shutdown_dst "$v_screen_master"
+  f_tail_server_log "$v_screen_master"
   sleep 10s
   update_dst
-  start_dst "$screen_name_2"
-  start_dst "$screen_name_1"
+  f_start_dst "$v_screen_caves"
+  f_start_dst "$v_screen_master"
 else
   echo -e "\033[33m \t--help.main: Parameter must be [ initmaster | initcaves | shutdown | updatedst | restart | SUR ] \033[0m"
   echo -e "\033[33m \t   SUR : shutdown and updatedst and restart \033[0m"
