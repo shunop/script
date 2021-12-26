f657132613de4386b51a485b790a5d9c6d4a1623
shunop <74529141+shunop@users.noreply.github.com>
2021-12-26 19:18:42 +0800

    feat: 初始化centos

--- centos/init-centos7-environment.sh
+++ centos/init-centos7-environment.sh
@@ -7,12 +7,12 @@
 ## Description: Init centos environment
 
 function f_init_firewall() {
-#  systemctl start firewalld
-#  systemctl status firewalld
+  #  systemctl start firewalld
+  #  systemctl status firewalld
   ## 设置firewall开机启动
   systemctl enable firewalld
   ## 禁止firewall开机启动
-#  systemctl disable firewalld
+  #  systemctl disable firewalld
 }
 
 function f_init_docker() {
@@ -23,4 +23,76 @@ function f_init_pip() {
   yum -y install epel-release python-pip
 }
 
+## 初始化swap交换区
+function f_init_swap() {
+#  参考 https://blog.csdn.net/duokongshi/article/details/80999077
+  if [ 0 -eq $(free | grep 'Swap' | awk '$2>1024*1024*1' | wc -l) ]; then
+    echo '开启 Swap'
+    ## 大约 5G
+    dd if=/dev/zero of=/swapfile bs=1024 count=5120k
+    mkswap /swapfile
+    swapon /swapfile
+    ## 持久化swap memory
+    echo '/swapfile          swap            swap    defaults        0 0      # for centos 6' >>/etc/fstab
+    chmod 600 /swapfile
+    free -h
+  else
+    echo '已经开启，无需重新操作'
+  fi
+}
 
+function f_config_openjdk() {
+#  配置 Open-JDK 的环境变量
+#  yum search java-11-openjdk									# 可以不用执行，查找安装包
+#  yum install -y java-11-openjdk java-11-openjdk-devel		# 安装
+  echo '配置 Open-JDK'
+  cat >> /etc/profile <<EOF
+
+# Open-Java Environment
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-11.0.12.0.7-0.el7_9.x86_64
+export JRE_HOME=\$JAVA_HOME/jre
+export CLASSPATH=.:\$JAVA_HOME/lib/dt.jar:\$JAVA_HOME/jre/lib/tools.jar:\$JRE_HOME/lib:\$CLASSPATH
+export PATH=\$JAVA_HOME/bin:\$PATH
+
+EOF
+  source /etc/profile
+}
+function f_config_oraclejdk() {
+#  配置 Oracle-JDK 的环境变量
+  echo '配置 Oracle-JDK'
+  cat >> /etc/profile <<EOF
+
+# Oracle-Java Environment
+export JAVA_HOME=/usr/local/java/jdk1.8.0_201
+export JRE_HOME=\${JAVA_HOME}/jre
+export CLASSPATH=.:\${JAVA_HOME}/lib:\${JRE_HOME}/lib
+export PATH=\${JAVA_HOME}/bin:\$PATH
+
+EOF
+  source /etc/profile
+}
+
+
+function f_init_java() {
+  v_arch=$(uname -m)
+  v_x86_64='x86_64'
+  echo 'cpu架构是: $(uname -m)'
+#  mkdir ~/wget && cd ~/wget
+#  wget -c https://download.oracle.com/otn/java/jdk/8u221-b11/230deb18db3e4014bb8e3e8324f81b43/jdk-8u221-linux-x64.tar.gz
+  mkdir /usr/local/java
+  mkdir ~/myupload
+  md5sum ~/myupload/jdk-8u201-linux-x64.tar.gz
+#  f4198016c840e227bb185bb1c1042a9f
+  tar -zxvf ~/myupload/jdk-8u201-linux-x64.tar.gz -C /usr/local/java/
+  # 创建软链接
+  ln -s /usr/local/java/jdk1.8.0_201/bin/java /usr/bin/java
+}
+
+# 初始化防火墙
+f_init_firewall
+# 初始化swap
+f_init_swap
+# 初始化java
+f_init_java
+# 配置环境变量
+f_config_oraclejdk
